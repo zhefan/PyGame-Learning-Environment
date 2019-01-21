1d37317437f3a3caf2e36cb3f198a82e8a159111
Zhefan Ye <zhefanye@gmail.com>
2019-01-21 15:58:57 -0500

    vertical move range 0~100, 100 steps, horizontal move range 0~100, 100 steps to hit target

--- ple/games/simpleshooter.py
+++ ple/games/simpleshooter.py
@@ -168,7 +168,7 @@ class Player(pygame.sprite.Sprite):
 
 
 class SimpleShooter(PyGameWrapper):
-    def __init__(self, width=102, height=100, target_speed_ratio=1,
+    def __init__(self, width=102, height=101, target_speed_ratio=1,
                  players_speed_ratio=1, bullet_speed_ratio=1, MAX_STEPS=10000):
 
         actions = {
