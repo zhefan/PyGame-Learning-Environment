7a63869455ddb89729ad680d7622b36eb6c675b5
Zhefan Ye <zhefanye@gmail.com>
2019-01-30 15:45:32 -0500

    reduce max steps to 100

--- ple/games/simpleshooter.py
+++ ple/games/simpleshooter.py
@@ -169,7 +169,7 @@ class Player(pygame.sprite.Sprite):
 
 class SimpleShooter(PyGameWrapper):
     def __init__(self, width=22, height=21, target_speed_ratio=1,
-                 players_speed_ratio=1, bullet_speed_ratio=1, MAX_STEPS=10000):
+                 players_speed_ratio=1, bullet_speed_ratio=1, MAX_STEPS=100):
 
         actions = {
             "up": K_UP,
